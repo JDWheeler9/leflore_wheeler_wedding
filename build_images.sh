9a66742ec8ac58b81cd0c42bdad599e54ae28fe9
Jeff Wheeler <jeffwheeler95@gmail.com>
2025-10-05 20:01:16 -0400

    typo in docker build script

--- build_images.sh
+++ build_images.sh
@@ -2,5 +2,5 @@ echo "Building and Pushing Prod Image... "
 docker buildx build --platform linux/arm/v8 -t jwheeler9/leflore_wheeler_wedding:latest . --push
 echo "Prod Image done"
 echo "Building Dev Image..."
-docker build -t jwheeler9/leflore_wheeler_wedding:dev --push
+docker build -t jwheeler9/leflore_wheeler_wedding:dev . --push
 echo "Dev Image done"
